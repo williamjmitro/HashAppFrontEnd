784ef1e8fb700604af045094018dcfc64a6935ce
Bill Mitro <williamjmitro@gmail.com>
2016-10-08 17:19:24 -0400

    who knows any more

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.get('/', function(request, response) {
   response.render('index.html');
 });
