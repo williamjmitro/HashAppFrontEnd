1efe683e9730fcd82f07d92d4f46155b098deb5a
Bill Mitro <williamjmitro@gmail.com>
2016-10-08 16:49:36 -0400

    trying something

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
-}).listen(process.env.PORT || 3000);
